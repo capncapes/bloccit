ed919993468f3df1c3485c4c4f8b65ab809a5bb6
Brandon Caples <brandoncaples@gmail.com>
2017-08-17 02:02:34 +0000

    Fixed root route issue

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  
+  def my_name
+      "Brandon"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'welcome/faq'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'welcome#index'
 end
