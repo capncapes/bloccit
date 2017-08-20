1432408b247ceedc78feb0add793d092546afb2f
Brandon Caples <brandoncaples@gmail.com>
2017-08-20 19:25:56 +0000

    Added SPAM catcher

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
     has_many :comments
+    
+    spam_id = 1
+    Post.update(spam_id, :title => 'SPAM')
+    
+    Post.all.each do |post|
+        spam_id = post.id if post.id % 5 == 0
+        Post.update(spam_id, :title => 'SPAM')
+    end
 end
