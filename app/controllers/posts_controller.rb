0780623e0ef6e0ec029d630bd9d6af464a0de32c
Brandon Caples <brandoncaples@gmail.com>
2017-08-22 01:16:24 +0000

    Moved censor functionality to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,12 @@ class PostsController < ApplicationController
 
   def edit
   end
+  
+  spam_id = 1
+  Post.update(spam_id, :title => 'SPAM')
+    
+  Post.all.each do |post|
+    spam_id = post.id if post.id % 5 == 0
+    Post.update(spam_id, :title => 'SPAM')
+  end
 end
