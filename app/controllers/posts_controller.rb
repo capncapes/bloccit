bc34cdda27be60e805d71ecd080935dd931a43f4
Brandon Caples <brandoncaples@gmail.com>
2017-08-22 18:26:27 +0000

    Moved SPAM censorship inside index method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,13 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    
+    spam_id = 1
+    Post.update(spam_id, :title => 'SPAM')
+    Post.all.each do |post|
+      spam_id = post.id if post.id % 5 == 0
+      Post.update(spam_id, :title => 'SPAM')
+    end
   end
 
   def show
@@ -10,13 +17,5 @@ class PostsController < ApplicationController
   end
 
   def edit
-  end
-  
-  spam_id = 1
-  Post.update(spam_id, :title => 'SPAM')
-    
-  Post.all.each do |post|
-    spam_id = post.id if post.id % 5 == 0
-    Post.update(spam_id, :title => 'SPAM')
   end
 end
