4694f2719ee8b23ee87cfa614d6e93a20d58dee6
Brandon Caples <brandoncaples@gmail.com>
2017-09-05 23:54:21 +0000

    Updated confirmation functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,11 @@ class UsersController < ApplicationController
         @user = User.new
         @user.name = params[:user][:name]
         @user.email = params[:user][:email]
-        @user.password = params[:user][:password]
-        @user.password_confirmation = params[:user][:password_confirmation]
     end
+
     
     def create
-        @user = User.new
-        @user.name = params[:user][:name]
-        @user.email = params[:user][:email]
-        @user.password = params[:user][:password]
-        @user.password_confirmation = params[:user][:password_confirmation]
+        @user = User.new(user_params)
         
         if @user.save
             flash[:notice] = "Welcome to Bloccit, #{@user.name}!"
@@ -25,5 +20,11 @@ class UsersController < ApplicationController
             flash.now[:alert] = "There was an error creating your account. Please try again."
             render :new
         end
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
