1aa6fd408385023c7f662207a70ad9e1b6edd13e
Brandon Caples <brandoncaples@gmail.com>
2017-09-30 20:58:52 +0000

    Updated posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
 require 'random_data'
+
+# Create Users
+5.times do
+    User.create!(
+        name: RandomData.random_name,
+        email: RandomData.random_email,
+        password: RandomData.random_sentence
+    )
+end
+users = User.all
 
 # Create Topics
 15.times do
@@ -20,6 +25,7 @@ topics = Topic.all
 # Create Posts
 50.times do
     Post.create!(
+        user: users.sample,
         topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
@@ -54,7 +60,14 @@ end
     )
 end
 
+user = User.first
+user.update_attributes!(
+    email: 'brandoncaples@gmail.com',
+    password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{SponsoredPost.count} sponsored posts created"
