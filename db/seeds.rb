42e79a3543beb08c9b1c43b97c1372d51113e4a0
Brandon Caples <brandoncaples@gmail.com>
2017-10-17 02:17:23 +0000

    Added voting features

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,12 +24,15 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-    Post.create!(
+    post = Post.create!(
         user: users.sample,
         topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
     )
+    
+    post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+    rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 posts = Post.all
 
@@ -82,4 +85,5 @@ puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
 puts "#{Question.count} questions created"
