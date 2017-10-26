2f7338f8dcf8d35f35b1e17aae8e4453919c0524
Brandon Caples <brandoncaples@gmail.com>
2017-10-26 05:29:23 +0000

    Added public profiles

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-    let(:my_user) { User.create!(name: "Bloc Head", email: "blochead@bloc.io", password: "password") }
+    let(:my_user) { create(:my_user) }
     
     describe "GET new" do
         it "returns http success" do
