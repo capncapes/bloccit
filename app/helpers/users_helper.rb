bf308ca875a4cf7229529411b63969547de1a91e
Brandon Caples <brandoncaples@gmail.com>
2017-11-01 04:51:06 +0000

    Added comments & votes factories

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+    def user_posts(user)
+        count = user.posts.count
+        
+        if count == 0
+            "#{user.name} has not submitted any posts yet."
+        else
+            "Posts"
+        end
+    end
+    
+    def user_comments(user)
+        count = user.comments.count
+        
+        if count == 0
+            "#{user.name} has not submitted any comments yet."
+        else
+            "Comments"
+        end
+    end
 end
---
+++ spec/factories/votes.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+    factory :vote do
+        post
+        user
+    end
+end
